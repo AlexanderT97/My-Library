d438d8ebe0deb74033c39404f0e42b9842e494cc
alex <alex-tischler@gmx.at>
2025-06-30 09:00:23 +0200

    CMS Security Token

--- src/lib/directus.js
+++ src/lib/directus.js
@@ -2,5 +2,7 @@ import { createDirectus, rest } from '@directus/sdk';
 
 const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL).with(rest());
 
+await directus.auth.static(import.meta.env.DIRECTUS_TOKEN);
+
 export default directus;
 
